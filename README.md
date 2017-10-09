3a15535b0f802c4c6b03e25ee27b765019e71bfa
KsGin <imqqyangfan@gmail.com>
2017-10-09 09:19:42 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# C语言词法分析器
+
+**一&emsp; C语言词法结构**
+1. C词法内容
+    + C语言标识符
+        
+        控制标识符共有 *do* , *while* , *if* , *else* , *switch* , *case*
+        
+        类型标识符共有 *int* , *float* , *long* , *unsigned* , *short* , *double*
+
+**二&emsp; 项目文件描述**
+1. 待填写
---
+++ cmake-build-debug/CMakeFiles/C_Lexical_Analyzer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Lexical_Analyzer.dir/main.cpp.o"
+  "C_Lexical_Analyzer.pdb"
+  "C_Lexical_Analyzer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_Lexical_Analyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

9bbf6b4dab5182d87ef90c24c22390c2822d4574
KsGin <imqqyangfan@gmail.com>
2017-10-12 20:25:09 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <sstream>
+
+std::string ReadFile(const std::string &file_path){
+    std::ifstream in(file_path);
+    std::istreambuf_iterator<char> beg(in), end;
+    std::string str(beg, end);
+    in.close();
+    return str;
+}
+
+
+void WriteFile(const std::string &file_path , const std::string &text){
+    std::ofstream out(file_path);
+    out << text;
+    out.close();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
